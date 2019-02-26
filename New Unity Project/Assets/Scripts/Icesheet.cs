8071c731b24caf6bcc7e78c79d1914eb4c1f3f5a
annawenger <45820274+annawenger@users.noreply.github.com>
2019-02-26 10:26:25 +0100

    create Icesheet-script for moving and spawning Icesheet, but is not finished yet

---
+++ New Unity Project/Assets/Scripts/Icesheet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Icesheet : MonoBehaviour {
+
+    public float speed;
+    public int direction;
+
+	// Use this for initialization
+	void Start () 
+    {
+        speed = 1f;
+        direction = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MovePlatform(speed, direction);	
+	}
+
+    public void MovePlatform (float mySpeed, int myDirection)
+    {
+        Debug.Log("MovePlatform");
+    }
+}
