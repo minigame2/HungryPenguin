090d539a7d4881c90dea5629ca29f48adc0b8115
annawenger <45820274+annawenger@users.noreply.github.com>
2019-02-26 10:00:34 +0100

    Scenes are biult,movement of Player is working, SceneLoader is working, Start- & TryAgain-Buttons are working

---
+++ New Unity Project/Assets/Scripts/ContactWithFloor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ContactWithFloor : MonoBehaviour
+{
+    private const string TAGPLAYER = "Player";
+    private const string TAGFLOOR = "Floor";
+ 
+    public SceneLoader SceneLoader;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == TAGPLAYER && gameObject.tag == "Floor")
+        {
+            SceneLoader.LoadGameOverScene();
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private const string AXISHORIZONTAL = "Horizontal";
+
+    //configruation parameters
+    public float moveSpeed = 8.0f;
+    public float xMin = -8.0f, xMax = 8.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis(AXISHORIZONTAL) * Time.deltaTime * moveSpeed;
+        var newPosX = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+
+        transform.position = new Vector2(newPosX, transform.position.y);
+    }
+}
+
+
